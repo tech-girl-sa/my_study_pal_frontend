5003a437f52f6d5d0460629a1b901f9b90268c8d
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-08-25 14:13:52 +0200

    add formik validation

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,11 +1,17 @@
 import classes from './Input.module.css'
+import { useField } from 'formik';
 
-function Input({label,id, ...props}){
+function Input({label, ...props}){
+    const [field, meta] = useField(props);
 
     return(
         <>
-        <label className={classes.customInputLabel} htmlFor={id}>{label}</label>
-        <input className={classes.customInput} id={id} {...props} />
+        <label className={classes.customInputLabel} htmlFor={props.name}>{label}</label>
+        <input className={`${classes.customInput} ${meta.touched && meta.error ? classes.error:''}`} 
+        {...props} {...field} />
+        {meta.touched && meta.error ? (
+         <div className={classes.errorMessage}>{meta.error}</div>
+       ) : null}
       </>
     )
 }
--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -1,24 +1,61 @@
 import classes from './Signup.module.css'
-import { useFormik } from 'formik';
+import { useFormik, Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import Card from '../common/Card';
 import Input from '../common/Input';
 import BlackButton from '../common/BlackButton';
+
+const initialValues={
+  fullName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+}
+const validationSchema=Yup.object({
+  fullName: Yup.string()
+    .max(150, 'Must be 150 characters or less')
+    .required('This field is required'),
+  email: Yup.string().email('Invalid email address').required('This field is required'),
+  password: Yup.string().min(6, 'Must be at least 6 chars').required('This field is required'),
+  confirmPassword: Yup.string().min(6, 'Must be at least 6 chars')
+  .required('This field is required')
+  .when("password", (password, schema) => {
+    return schema.test({
+      test: (value) => password === value,
+      message: "Passwords must match",
+    });
+  }),
+})
+
 
 function Signup(){
 
     return(
         <Card>
     <h1>Create Your Study Pal Account</h1>
-    <form class={classes.signupForm}>
-      <Input label="Full Name" type="text" id="name" name="name" required  ></Input>
-      <Input label="Email" type="email" id="email" name="email" required  ></Input>
-      <Input label="Password" type="password" id="password" name="password" required  ></Input>
-      <Input label="Confirm Password" type="password" id="confirmPassword" name="confirmPassword" required  ></Input>
+    <Formik
+       initialValues={initialValues}
+       validationSchema={validationSchema}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+    <Form className={classes.signupForm} >
+      <Input 
+      label="Full Name" type="text" id="fullName" name="fullName" ></Input>
+      
+      <Input label="Email" type="email" id="email" name="email" ></Input>
+      <Input label="Password" type="password" id="password" name="password" ></Input>
+      <Input label="Confirm Password" type="password"
+      id="confirmPassword" name="confirmPassword"></Input>
 
       <BlackButton type="submit">Create Account</BlackButton>
-      <p class={classes.loginLink}>Already have an account? <a href="#">Log in</a></p>
-    </form>
+      <p className={classes.loginLink}>Already have an account? <a href="#">Log in</a></p>
+    </Form>
+    </Formik>
     </Card>
     )
 };
