c23a79d3852bba02cad9291dcfb34e21e307765d
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-08-31 22:47:56 +0200

    add links to progress bar and reflect active statuses

--- src/components/pages/onboarding/OnboardingLayout.jsx
+++ src/components/pages/onboarding/OnboardingLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet,NavLink } from "react-router-dom";
 import classes from './OnboardingLayout.module.css';
 import { useEffect } from "react";
 import robotHeadLeft from "../../../assets/robot_head_left.png";
@@ -16,13 +16,21 @@ export default function OnboardingLayout() {
       <>
         <div className={classes.progressWrapper}>
           <div className={classes.progressBar}>
-            <div className={`${classes.step} ${classes.stepActive}`}>1</div>
+              <NavLink  className={({ isActive }) =>
+     isActive ? `${classes.step} ${classes.stepActive}` : classes.step
+  } to="step1">1</NavLink>
             <div className={classes.bar}></div>
-            <div className={classes.step}>2</div>
+            <NavLink  className={({ isActive }) =>
+     isActive ? `${classes.step} ${classes.stepActive}` : classes.step
+  } to="step2">2</NavLink>
             <div className={classes.bar}></div>
-            <div className={classes.step}>3</div>
+            <NavLink  className={({ isActive }) =>
+     isActive ? `${classes.step} ${classes.stepActive}` : classes.step
+  } to="step3">3</NavLink>
             <div className={classes.bar}></div>
-            <div className={classes.step}>4</div>
+            <NavLink  className={({ isActive }) =>
+     isActive ? `${classes.step} ${classes.stepActive}` : classes.step
+  } to="step4">4</NavLink>
           </div>
         </div>
   
