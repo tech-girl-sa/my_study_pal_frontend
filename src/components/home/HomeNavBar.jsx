b717245ec66c4abd81cbf7eb8ae9cc2404f045fa
tech-girl-sa <sarramabrouk3@gmail.com>
2025-09-02 19:26:51 +0200

    add links and login page logic

--- src/components/home/HomeNavBar.jsx
+++ src/components/home/HomeNavBar.jsx
@@ -1,14 +1,15 @@
 import logo from "../../assets/logo.png";
-import classes from './HomeNavBar.module.css'
+import classes from './HomeNavBar.module.css';
+import { NavLink } from "react-router-dom";
 
 export default function HomeNavBar(){
     return <nav className={classes.navbar}>
     <div className={classes.logo}> <img src={logo} alt="Study Pal logo"/></div>
     <ul className={classes.navLinks}>
-        <li><a href="#">Home</a></li>
+        <li><NavLink to="/home">Home</NavLink></li>
         <li><a href="#">Features</a></li>
-        <li><a href="#">Login</a></li>
-        <li><a href="signup">Sign Up</a></li>
+        <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/signup">Signup</NavLink></li>
     </ul>
     </nav>
 }
