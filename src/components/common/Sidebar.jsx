fb0dcefebb47451f5f33a0020e2ba797743c6d73
tech-girl-sa <sarramabrouk3@gmail.com>
2025-09-08 22:59:35 +0200

    add correct links to sidebar

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,14 +1,15 @@
 import classes from './Sidebar.module.css';
 import { FaHome, FaBook, FaCopy, FaFolderOpen, FaPenNib, FaCog } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 
 export default function Sidebar(){
 
     return  <aside className={classes.sidebar}>
     <nav className={classes.nav}>
-      <a href="/main/dashboard" class="nav-link active"><FaHome/> Home</a>
-      <a href="/main/subjects"><FaBook/> Subjects</a>
-      <a href="/main/courses"><FaCopy/> Courses</a>
-      <a href="/main/documents"><FaFolderOpen/> Documents</a>
+      <NavLink to="/dashboard" class="nav-link active"><FaHome/> Home</NavLink>
+      <NavLink to="/dashboard/subjects"><FaBook/> Subjects</NavLink>
+      <NavLink to="/dashboard/courses"><FaCopy/> Courses</NavLink>
+      <NavLink to="/dashboard/documents"><FaFolderOpen/> Documents</NavLink>
       <a href="/main/quizzes"><FaPenNib/> Quizzes</a>
       <a href="#"><FaCog/>Settings</a>
     </nav>
