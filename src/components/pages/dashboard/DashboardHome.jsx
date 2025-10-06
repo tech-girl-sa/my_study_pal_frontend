8bbd2a4a411ba343e84f81e5640fe702d42fc97b
tech-girl-sa <mabrouksarra3@gmail.com>
2025-10-06 16:33:19 +0100

    make deshboard gome data dynamic

--- src/components/pages/dashboard/DashboardHome.jsx
+++ src/components/pages/dashboard/DashboardHome.jsx
@@ -3,82 +3,30 @@ import ChatWidgetSection from "../../Dashboard components/ChatWidgetSection";
 import SubjectCard from "../../Dashboard components/SubjectCard";
 import Section from "../../common/Section";
 import Table from '../../common/Table';
-
-const subjects = [
-  {
-    "title": "Mathematics",
-    "courses_count": 10,
-    "description": "A wide range of topics from algebra to calculus.",
-    "documents_count": 8,
-    "quizzes_count": 5
-  },
-  {
-    "title": "Science",
-    "courses_count": 8,
-    "description": "Explore the fascinating world of physics, chemistry, and biology.",
-    "documents_count": 12,
-    "quizzes_count": 6
-  },
-  {
-    "title": "Literature",
-    "courses_count": 15,
-    "description": "Delve into classic and modern literature from various authors.",
-    "documents_count": 10,
-    "quizzes_count": 4
-  }
-]
-const courses = [
-  {
-    id: 1,
-    title: "Advanced Calculus",
-    subject: "Mathematics",
-    link: "#",
-  },
-  {
-    id: 2,
-    title: "Organic Chemistry",
-    subject: "Science",
-    link: "#",
-  },
-  {
-    id: 3,
-    title: "Shakespearean Literature",
-    subject: "Literature",
-    link: "#",
-  },
-  {
-    id: 4,
-    title: "Data Structures and Algorithms",
-    subject: "Computer Science",
-    link: "#",
-  },
-  {
-    id: 5,
-    title: "Modern Art History",
-    subject: "Arts",
-    link: "#",
-  },
-];
+import { useGetCourses, useGetSubjects } from '../../../utils/hooks';
+import { NavLink } from 'react-router-dom';
 
 
 
 export default function DashboardHome(){
+   const {data:subjects, isLoading, error} = useGetSubjects()
+   const {data:courses}= useGetCourses()
     return <>
     
     <ChatWidgetSection/>
     <Section title="Recent Subjects" icon="book">
   <div className={classes.cardContainer}>
-    {subjects.map(subject=><SubjectCard subject={subject} key={subject.id}/>)}
+    {subjects?.map(subject=><SubjectCard subject={subject} key={subject.id}/>)}
     </div>
   </Section >
   
   <Section title="Recent Courses" icon="copy" >
  
  <Table headers={["Course Title", "Subject", "View Course"]}>
-        {courses.map(course=> (<tr id={course.id}>
+        {courses?.map(course=> (<tr id={course.id}>
           <td  className={classes.courseTitle}><a href="#">{course.title}</a></td>
           <td>{course.subject}</td>
-          <td><a href="#" class="view-course-link">View Course</a></td>
+          <td><NavLink to={`/dashboard/courses/${course.id}/${course.first_section_id}`} class="view-course-link">View Course</NavLink></td>
         </tr>))}
    </Table>
   </Section>
